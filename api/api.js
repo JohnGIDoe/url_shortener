f74f67c96157e891918b6fa729d5a78d7fbca40b
Piotr Grzybowski <grzybek6@gmail.com>
2019-02-22 17:01:39 +0000

    Added new schema to mongoDB and skeleton of shorten function to api.js and index.js

--- api/api.js
+++ api/api.js
@@ -1,7 +1,24 @@
 const mongoose = require("mongoose");
 
-const DB_USER = "user";
-const DB_PASSWORD = "password";
+const DB_USER = "admin";
+const DB_PASSWORD = "haslo123";
 const WEBSITE_URL = "http://localhost:8080/";
 
-mongoose.connect(`podstaw tutaj adres z Twojego mlab.com`);
+mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@ds147225.mlab.com:47225/basedata`);
+
+let schema = new mongoose.Schema({
+  short: String,
+  url: String
+});
+
+let URL = mongoose.model("URL", schema);
+
+function shortenURL(url, cb) {
+
+
+
+}
+
+module.exports = {
+  shorten: shortenURL
+}
